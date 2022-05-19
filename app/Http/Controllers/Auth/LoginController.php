69d35d7c624d0e08277a2b89f170d76a4f6d7881
Dedi Setiawan <69558728+dedi12pl@users.noreply.github.com>
2022-05-19 08:11:47 +0800

    ds

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -96,27 +96,4 @@ class LoginController extends Controller
     public function getChangePassword(){
         return view('user.changePassword');
     }
-
-    public function changePassword(Request $request)
-    {
-        $user = Auth::user();
-    
-        $userPassword = $user->password;
-        
-        $request->validate([
-            'current_password' => 'required',
-            'password' => 'required|same:confirm_password|min:6',
-            'confirm_password' => 'required',
-        ]);
-
-        if (!Hash::check($request->current_password, $userPassword)) {
-            return back()->withErrors(['current_password'=>'password not match']);
-        }
-
-        $user->password = Hash::make($request->password);
-
-        $user->save();
-
-        return redirect()->back()->with('success','password successfully updated');
-    }
 }
